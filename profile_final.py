f78e6dda665e931a08502315c89c2a9b027d0ba4
AhsanAAR <ahsanabdurrehman@gmail.com>
2026-01-23 10:51:51 +0100

    cube + profiling final"

--- profile_final.py
+++ profile_final.py
@@ -9,20 +9,23 @@ if __name__ == "__main__":
 
     K = 10
 
-    for n in range(10, 30):
+    for n in range(12, 30):
         total_time_unique = 0
         total_hints_unique = 0
         total_time_random = 0
         total_hints_random = 0
 
-        for k in range(K):
-            _, unique_hints, unique_time = final(n, True)
-            _, random_hints, random_time = final(n, False)
+        hints_start = int(n*n*0.2)
 
-            total_time_unique += unique_time
-            total_hints_unique += unique_hints
+        for k in range(K):
+            print(k)
+            # _, unique_hints, unique_time = final(n, True, hints_start=hints_start)
+            _, random_hints, random_time = final(n, False, hints_start=hints_start)
+
+            # total_time_unique += unique_time
+            # total_hints_unique += unique_hints
 
             total_time_random += random_time
             total_hints_random += random_hints
         
-        print(n, K, total_time_unique/K, total_hints_unique/K, total_time_random/K, total_hints_random/K)
+        print(n, hints_start, total_time_unique/K, total_hints_unique/K, total_time_random/K, total_hints_random/K)
---
+++ solve_z3_cube.py
@@ -0,0 +1,124 @@
+import z3 as z
+# core and satisfying subsetes z3 microsoft.xs
+
+def all_smt(s, initial_terms):
+    def block_term(s, m, t):
+        s.add(t != m.eval(t, model_completion=True))
+    def fix_term(s, m, t):
+        s.add(t == m.eval(t, model_completion=True))
+    def all_smt_rec(terms):
+        if z.sat == s.check():
+           m = s.model()
+           yield m
+           for i in range(len(terms)):
+               s.push()
+               block_term(s, m, terms[i])
+               for j in range(i):
+                   fix_term(s, m, terms[j])
+               yield from all_smt_rec(terms[i:])
+               s.pop()   
+    yield from all_smt_rec(list(initial_terms))
+
+def solve_z3(write_ans=False, list_all=False):
+    with open('input.txt', 'r') as file:
+        N = int(file.readline())
+
+        top_vals = list(map(int, file.readline().split(',')))
+        right_vals = list(map(int, file.readline().split(',')))
+        bottom_vals = list(map(int, file.readline().split(',')))
+
+        top_ans = list(map(int, file.readline().split(',')))
+        left_ans = list(map(int, file.readline().split(',')))
+        bottom_ans = list(map(int, file.readline().split(',')))
+
+    top_face = [
+        [z.Bool(f"t_{r}_{c}") for c in range(N)]
+        for r in range(N)
+    ]
+    left_face = [
+        [z.Bool(f"l_{r}_{c}") for c in range(N)]
+        for r in range(N)
+    ]
+    right_face = [
+        [z.Bool(f"r_{r}_{c}") for c in range(N)]
+        for r in range(N)
+    ]
+
+    top_row_vars = [
+        [z.Int(f"tr_{r}_{c}") for c in range(N)]
+        for r in range(N)
+    ]
+    top_col_vars = [
+        [z.Int(f"tc_{r}_{c}") for c in range(N)]
+        for r in range(N)
+    ]
+
+    left_row_vars = [
+        [z.Int(f"tr_{r}_{c}") for c in range(N)]
+        for r in range(N)
+    ]
+    left_col_vars = [
+        [z.Int(f"tc_{r}_{c}") for c in range(N)]
+        for r in range(N)
+    ]
+
+    right_row_vars = [
+        [z.Int(f"rr_{r}_{c}") for c in range(N)]
+        for r in range(N)
+    ]
+    right_col_vars = [
+        [z.Int(f"rc_{r}_{c}") for c in range(N)]
+        for r in range(N)
+    ]
+
+    solver = z.Solver()
+
+    for r in range(N):
+        for c in range(N):
+            solver.add(z.Implies(top_face[r][c], z.And(top_row_vars[r][c] == top_vals[c], top_col_vars[r][c] == bottom_vals[N-1-r])))
+            solver.add(z.Implies(left_face[r][c], z.And(left_row_vars[r][c] == top_vals[c], left_col_vars[r][c] == right_vals[r])))
+            solver.add(z.Implies(right_face[r][c], z.And(right_row_vars[r][c] == bottom_vals[c], right_col_vars[r][c] == right_vals[r])))
+          
+            solver.add(z.Implies(z.Not(top_face[r][c]), z.And(top_row_vars[r][c] == 0, top_col_vars[r][c] == 0)))
+            solver.add(z.Implies(z.Not(left_face[r][c]), z.And(left_row_vars[r][c] == 0, left_col_vars[r][c] == 0)))
+            solver.add(z.Implies(z.Not(right_face[r][c]), z.And(right_row_vars[r][c] == 0, right_col_vars[r][c] == 0)))
+          
+    for r in range(N):
+        solver.add(sum(row_vars[r]) == row_ans[r])
+
+    for c in range(N):
+        col_list = []
+        for r in range(N):
+            col_list.append(col_vars[r][c])
+        
+        solver.add(sum(col_list) == col_ans[c])
+
+    i = 0
+    if solver.check() == z.sat:
+        if write_ans:
+            open("z3_out.txt", "w").close()
+
+        
+        for model in all_smt(solver, [grid[r][c] for c in range(N) for r in range(N)]):
+            i+=1
+
+            if write_ans:
+                answer = [
+                    [model[grid[r][c]] for c in range(N)]
+                    for r in range(N)
+                ]
+
+                with open('z3_out.txt', 'a') as file:
+                    for r in answer:
+                        file.write(f'{",".join(map(lambda x: "1" if x else "0", r))}\n')
+            
+            if i == 2 and not list_all:
+                return 2
+            
+    else:
+        print("Not possible")
+    
+    return i
+
+if __name__ == "__main__":
+    solve_z3(True, True)
